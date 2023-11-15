c274dce69a9725f6a284bba6636423b799427bca
iherdt-mparticle <54378555+iherdt-mparticle@users.noreply.github.com>
2023-11-15 14:15:49 -0800

    Update Package.swift
    removed Firebase package

--- Package.swift
+++ Package.swift
@@ -14,10 +14,7 @@ let package = Package(
     dependencies: [
       .package(name: "mParticle-Apple-SDK",
                url: "https://github.com/mParticle/mparticle-apple-sdk",
-               .upToNextMajor(from: "8.0.0")),
-      .package(name: "Firebase",
-               url: "https://github.com/firebase/firebase-ios-sdk.git",
-               .upToNextMajor(from: "9.0.0")),
+               .upToNextMajor(from: "8.0.0"))
     ],
     targets: [
         .target(
